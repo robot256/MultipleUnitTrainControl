6be44346249d7a202fdd1f7c3ee6345a771f9a8c
robot256 <robot256@gmail.com>
2019-04-12 17:56:06 -0400

    Make RET support version dependent

--- prototypes/ret_add_mu.lua
+++ prototypes/ret_add_mu.lua
@@ -6,9 +6,15 @@
 
 if mods["Realistic_Electric_Trains"] then
 
-	-- Generate an MU version of the Electric and Electric Mk2 Locomotives
-	createMuLoco("ret-electric-locomotive","ret-electric-locomotive-mu","item-with-entity-data",false)
-	createMuLoco("ret-electric-locomotive-mk2","ret-electric-locomotive-mk2-mu","item-with-entity-data",false)
-	createMuLoco("ret-modular-locomotive","ret-modular-locomotive-mu","item-with-entity-data",false)
-	
+	local r = {0,4,2} -- required version
+	local ver = mods["Realistic_Electric_Trains"]
+	local f = {tonumber(string.match(ver,"^(%d+)%.")),  -- found version
+	           tonumber(string.match(ver,"%.(%d+)%.")),
+			   tonumber(string.match(ver,"%.(%d+)$"))}
+	if (f[1] > r[1]) or (f[1] == r[1] and f[2] > r[2]) or (f[1] == r[1] and f[2] == r[2] and f[3] >= r[3]) then
+		-- Generate an MU version of the Electric and Electric Mk2 Locomotives
+		createMuLoco("ret-electric-locomotive","ret-electric-locomotive-mu","item-with-entity-data",false)
+		createMuLoco("ret-electric-locomotive-mk2","ret-electric-locomotive-mk2-mu","item-with-entity-data",false)
+		createMuLoco("ret-modular-locomotive","ret-modular-locomotive-mu","item-with-entity-data",false)
+	end
 end
