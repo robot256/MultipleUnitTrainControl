7afe50dca16618ce13ba0d347a3990764f1ae259
robot256 <robot256@GMAIL.COM>
2020-12-06 13:17:10 -0500

    Revision for 0.4.8
    - Remove unnecessary remote interface call to Electronic Locomotives
    - Tab to space in a couple files
    - Typo in changelog

--- data/createMuLocoEntityPrototype.lua
+++ data/createMuLocoEntityPrototype.lua
@@ -8,30 +8,30 @@
 
 
 function createMuLocoEntityPrototype(name, newName, has_description, power_multiplier)
-	-- Copy source locomotive prototype
-	local oldLoco = data.raw["locomotive"][name]
-	local loco = table.deepcopy(oldLoco)
-	
-	-- Change name of prototype
-	loco.name = newName
-	-- Make this entity non-placeable (you're not allowed to have -mu items in your inventory), doesn't really work?
-	if(loco.flags["placeable-neutral"]) then
-		loco.flags["placeable-neutral"] = nil
-	end
-	if(loco.flags["placeable-player"]) then
-		loco.flags["placeable-player"] = nil
-	end
-	if(loco.flags["placeable-enemy"]) then
-		loco.flags["placeable-enemy"] = nil
-	end
-	
-	-- Make it so a normal locomotive can be pasted on this blueprint, doesn't really work?
-	loco.additional_pastable_entities = {name}
-	
-	-- Change the power level (string contains suffix "kW"). This also increases fuel consumption.
-	loco.max_power = multiply_energy_value(loco.max_power, power_multiplier)
-	
-	-- Concatenate the localized name and description string of the source loco with our template.
+  -- Copy source locomotive prototype
+  local oldLoco = data.raw["locomotive"][name]
+  local loco = table.deepcopy(oldLoco)
+  
+  -- Change name of prototype
+  loco.name = newName
+  -- Make this entity non-placeable (you're not allowed to have -mu items in your inventory), doesn't really work?
+  if(loco.flags["placeable-neutral"]) then
+    loco.flags["placeable-neutral"] = nil
+  end
+  if(loco.flags["placeable-player"]) then
+    loco.flags["placeable-player"] = nil
+  end
+  if(loco.flags["placeable-enemy"]) then
+    loco.flags["placeable-enemy"] = nil
+  end
+  
+  -- Make it so a normal locomotive can be pasted on this blueprint, doesn't really work?
+  loco.additional_pastable_entities = {name}
+  
+  -- Change the power level (string contains suffix "kW"). This also increases fuel consumption.
+  loco.max_power = multiply_energy_value(loco.max_power, power_multiplier)
+  
+  -- Concatenate the localized name and description string of the source loco with our template.
   if loco.localised_name then
     -- Original mod already set dynamic localised name, use it in our template
     loco.localised_name = {'template.mu-name',table.deepcopy(loco.localised_name)}
@@ -43,15 +43,15 @@ function createMuLocoEntityPrototype(name, newName, has_description, power_multi
   if loco.localised_description then
     -- Original mod already set dynamic localised description, use it in our template
     loco.localised_description = {'template.mu-description',table.deepcopy(loco.localised_description)}
-	elseif has_description==true then
+  elseif has_description==true then
     -- We know this mod set a description in their locale file (no way to determine dynamically at this phase)
-		loco.localised_description = {'template.mu-description',{'entity-description.'..name}}
-	else
+    loco.localised_description = {'template.mu-description',{'entity-description.'..name}}
+  else
     -- Assume there is no description in locale file
-		loco.localised_description = {'template.plain-mu-description'}
-	end
+    loco.localised_description = {'template.plain-mu-description'}
+  end
   
-	return loco
+  return loco
 end
 
 return createMuLocoEntityPrototype
