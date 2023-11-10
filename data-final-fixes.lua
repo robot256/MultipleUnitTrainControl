2d46d8f3921c5d0066311dc89b1aab054736c2a2
robot256 <robot256@GMAIL.COM>
2023-11-10 12:18:32 -0500

    Release 0.4.17

--- data-final-fixes.lua
+++ data-final-fixes.lua
@@ -1,19 +1,27 @@
 --[[ Copyright (c) 2022 robot256 (MIT License)
  * Project: Multiple Unit Train Control
  * File: data-final-fixes.lua
- * Description: Update fuel categories for MU locomotives to match the originals
+ * Description: Update fuel categories and grids for MU locomotives to match the originals
 --]]
 
 for name,loco in pairs(data.raw["locomotive"]) do
   -- Check if this is a MU or if it already has a MU
   if string.find(name, "%-mu$") ~= nil then
-    -- ends in MU, make sure regular loco exists. If not, then the vanilla loco ended with -mu, and the new one will be -mu-mu
+    -- ends in MU, make sure regular loco exists (without the last -mu).
     local basename = string.sub(name, 1, -4)
-    if data.raw["locomotive"][basename] and data.raw.locomotive[basename].burner then
-      -- This MU has a regular loco with burner, copy fuel categories to MU version
-      data.raw.locomotive[name].burner.fuel_category = data.raw.locomotive[basename].burner.fuel_category
-      -- Link the fuel_categories table so it gets updated here even if the base changes later, no dependencies required!
-      data.raw.locomotive[name].burner.fuel_categories = data.raw.locomotive[basename].burner.fuel_categories
+    if data.raw["locomotive"][basename] then
+    
+      -- Update fuel category
+      if data.raw.locomotive[basename].burner then
+        -- This MU has a regular loco with burner, copy fuel categories to MU version
+        data.raw.locomotive[name].burner.fuel_category = data.raw.locomotive[basename].burner.fuel_category
+        -- Link the fuel_categories table so it gets updated here even if the base changes later, no dependencies required!
+        data.raw.locomotive[name].burner.fuel_categories = data.raw.locomotive[basename].burner.fuel_categories
+      end
+    
+      -- Update grid assignment (or set to nil)
+      data.raw.locomotive[name].equipment_grid = data.raw.locomotive[basename].equipment_grid
     end
+    
   end
 end
